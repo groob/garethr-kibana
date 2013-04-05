02caee22e73a93f5c3234376619358abd44e3c9b
Rowe <drowe@ausmac748268.local>
2013-04-05 14:27:03 -0500

    Add a upstart script that works with rhel and work around a bug with puppet not allowing the upstart provider on rhel

--- manifests/service.pp
+++ manifests/service.pp
@@ -6,19 +6,35 @@ class kibana::service {
 
   file { '/etc/init/kibana.conf':
     ensure  => present,
-    content => template('kibana/etc/init/kibana.conf.erb')
+    content => $operatingsystem ? {
+      centos, redhat => template('kibana/etc/init/kibana-redhat.conf.erb'),
+      debian, ubuntu => template('kibana/etc/init/kibana.conf.erb')
+    }
   }
 
-  service { 'kibana':
-    ensure     => running,
-    enable     => true,
-    hasstatus  => true,
-    hasrestart => true,
-    provider   => upstart,
-    require    => [
-      File['/etc/init.d/kibana'],
-      File['/etc/init/kibana.conf'],
-    ],
+  case $operatingsystem {
+    centos, redhat: {
+      service { 'kibana':
+        ensure     => running,
+        hasstatus  => true,
+        hasrestart => true,
+        start      => '/sbin/initctl start kibana',
+        stop       => '/sbin/initctl stop kibana',
+        status     => '/sbin/initctl status kibana | grep "/running" 1>/dev/null 2>&1',
+      }
+    }
+    debian, ubuntu: {
+      service { 'kibana':
+        ensure     => running,
+        enable     => true,
+        hasstatus  => true,
+        hasrestart => true,
+        provider   => upstart,
+        require    => [
+          File['/etc/init.d/kibana'],
+          File['/etc/init/kibana.conf'],
+        ],
+     }
   }
 
   File['/etc/init/kibana.conf'] ~> Service['kibana']
